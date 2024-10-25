1f1631c4088bd5553974948f11e071d5265f9c0c
Otavio Antonio da Silva Junior <71262689+Otavio-Antonio@users.noreply.github.com>
2024-10-24 23:19:14 -0300

    Add files via upload

---
+++ Lab10/Ast/Testes/teste1.cpp
@@ -0,0 +1,17 @@
+/* exemplo de programa em 
+   linguagem simplificada */
+   
+int main()
+{
+  // variáveis
+  int i;
+  int j;
+  bool z;
+  bool h;
+
+  // atribuições
+  i = 15;
+  j = 30;
+  z = j;
+  h = i > 10 && j <= 20;
+}
