2283ee9aa078895ddb3f916c1844d2469845153a
Otavio Antonio da Silva Junior <71262689+Otavio-Antonio@users.noreply.github.com>
2024-11-02 12:00:01 -0300

    Add files via upload

---
+++ Lab11/Testes/teste5.cpp
@@ -0,0 +1,17 @@
+// meu programa de teste
+int main()
+{
+    int i; int j; int k; float vet[10]; bool flag;
+    i = 0;
+    j = 9;
+    k = 1;
+    flag = false;
+
+    while (i<j)
+    {
+        if(true){
+            i = i + 1;
+        }
+        
+    }
+}
